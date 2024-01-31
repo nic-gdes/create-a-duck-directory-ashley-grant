a1ccb093adbc5998ff3abe9ab29b7256d17d31ae
Ashley Grant <agrant2@nic.edu>
2024-01-31 15:47:37 -0800

    Added code in index.html and style in style.css

--- index.php
+++ index.php
@@ -1,50 +1,68 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>Ducks</title>
+</head>
+<body>
+<header>
+    <nav>
+        <img src="../assets/img/Duck Logo Black.png" alt="" />
+        <ul>
+            <li><a href="/">Home Page</a></li>
+            <li><a href="./contact.php">Duck Profile</a></li>
+        </ul>
+    </nav>
+</header>
+    <main>
+        <h1>Home Page</h1>
+        <h3>Featuring Amazing Ducks</h3>
+        <div class="grid">
+            <div class="grid-item">
+                <h1>Barbie Duck</h1>
+                <img class="grid-img" src="./assets/img/Barbie-Duck.png" alt="">
+                <p>Scuttle is a seagull (gannet in the live action adaptation) and friend of Ariel. They appear in the film as an expert on human objects with whom Ariel consults about items she salvages, though their identifications consist of nonsense.</p>
+            </div>
+            <div class="grid-item">
+                <h1>Cowboy Duck</h1>
+                <img class="grid-img" src="./assets/img/Cowboy-Duck.png" alt="">
+                <p>Ariel's most defining feature is her long, flowing, thick, bright red hair. From the waist down, she has a green horizontal fish-like tail with sea-green scales and sea-foam green fins and a matching V-shaped waist-line resembling fish-lips that reveals her midriff and her belly button.</p>
+            </div>
+            <div class="grid-item">
+                <h1>Barbie Duck</h1>
+                <img class="grid-img" src="./assets/img/Barbie-Duck.png" alt="">
+                <p>Horatio Thelonious Ignacious Crustaceous Sebastian is a red Jamaican crab and a servant of King Triton, and also his main musical composer. His main song is "Under the Sea". He is voiced by Samuel E. Wright in the films, the TV series and the Sebastian segments from the Marsupilami series.</p>
+            </div>
+            <div class="grid-item">
+                <h1>Cowboy Duck</h1>
+                <img class="grid-img" src="./assets/img/Cowboy-Duck.png" alt="">
+                <p>Ursula, also known as The Sea Witch or Vanessa, is the main antagonist of the 2023 musical fantasy film The Little Mermaid, the live-action remake of the 1989 animated feature film of the same name. She is a cecaelia sea witch and the younger sister of King Triton.</p>
+            </div>
+            <div class="grid-item">
+                <h1>Barbie Duck</h1>
+                <img class="grid-img" src="./assets/img/Barbie-Duck.png" alt="">
+                <p>Ursula, also known as The Sea Witch or Vanessa, is the main antagonist of the 2023 musical fantasy film The Little Mermaid, the live-action remake of the 1989 animated feature film of the same name. She is a cecaelia sea witch and the younger sister of King Triton.</p>
+            </div>
+            <div class="grid-item">
+                <h1>Cowboy Duck</h1>
+                <img class="grid-img" src="./assets/img/Cowboy-Duck.png" alt="">
+                <p>Ursula, also known as The Sea Witch or Vanessa, is the main antagonist of the 2023 musical fantasy film The Little Mermaid, the live-action remake of the 1989 animated feature film of the same name. She is a cecaelia sea witch and the younger sister of King Triton.</p>
+            </div>
+        </div>
+    </main>
+    <div class="footer-style">
+    <nav>
+        <ul>
+            <li><a href="/">Home Page</a></li>
+            <li><a href="./contact.php">Duck Profile</a></li>
+        </ul>
+    </nav>
 
-/******** CONNECT TO A DATABASE *********/
-// use mysqli_connect to connect to database. There are four arguments: host, username, password, and database name, in that order.
-
-$conn = mysqli_connect("db:3306", "db", "db", "db");
-
-// check if there's no connection and give an error if so
-
-if (mysqli_connect_errno()) {
-    echo "Connection error: " . mysqli_connect_error();
-    exit();
-}
-
-/******** EXECUTE QUERIES ON THE DATABASE TO RETRIEVE DATA *********/
-// write query for all records
-
-$sql = "SELECT * FROM example";
-// $sql = "SELECT name, favorite_foods, image FROM example ORDER BY first_name";
-
-// make query and get result
-
-$result = mysqli_query($conn, $sql);
-
-// fetch the resulting rows as an array
-
-$data = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-/******** WRAP UP DATABASE CONNECTION *********/
-// free the result from memory
-
-mysqli_free_result($result);
-
-// close the connection to the database
-
-mysqli_close($conn);
-
-print_r($data);
-
-?>
-
-
-<h1>Hello World</h1>
-
-<ul>
-    <?php foreach($data as $name) : ?>
-    <li><?php echo $name['first_name']; ?></li>
-    <!-- <li><?php echo $name['id'] . ": " . $name['first_name']; ?></li> -->
-    <?php endforeach ?>
-</ul>
+    <footer>
+        &copy; <?php echo date("Y") ?> GDES. All Rights Reserved.
+    </footer>
+</div>
+</body>
+</html>
