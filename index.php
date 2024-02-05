13746fe916d537b3db6a92c03090382487c687b5
Ashley Grant <agrant2@nic.edu>
2024-02-04 20:06:44 -0800

    comment out width

--- index.php
+++ index.php
@@ -18,37 +18,37 @@
 </header>
     <main>
         <h1 class="home-page-title">Home Page</h1>
-        <h3>Featuring Amazing Ducks</h3>
+        <h3 class="home-page-subtitle">Featuring Amazing Ducks</h3>
         <div class="grid">
             <div class="grid-item">
                 <h1>Barbie Duck</h1>
                 <img class="grid-img" src="./assets/img/Barbie-Duck.png" alt="Rubber Duck that looks like Barbie">
-                <p>Scuttle is a seagull (gannet in the live action adaptation) and friend of Ariel. They appear in the film as an expert on human objects with whom Ariel consults about items she salvages, though their identifications consist of nonsense.</p>
+                <p>This ducky's favorite food is Crumpets with Honey</p>
             </div>
             <div class="grid-item">
                 <h1>Cowboy Duck</h1>
                 <img class="grid-img" src="./assets/img/Cowboy-Duck.png" alt="Rubber Duck that looks like a cowboy">
-                <p>Ariel's most defining feature is her long, flowing, thick, bright red hair. From the waist down, she has a green horizontal fish-like tail with sea-green scales and sea-foam green fins and a matching V-shaped waist-line resembling fish-lips that reveals her midriff and her belly button.</p>
+                <p>This ducky's favorite foods are Beans and Bread</p>
             </div>
             <div class="grid-item">
-                <h1>Barbie Duck</h1>
+                <h1>Einstein Duck</h1>
                 <img class="grid-img" src="./assets/img/Einstein-Duck.png" alt="Rubber Duck that looks like Einstein">
-                <p>Horatio Thelonious Ignacious Crustaceous Sebastian is a red Jamaican crab and a servant of King Triton, and also his main musical composer. His main song is "Under the Sea". He is voiced by Samuel E. Wright in the films, the TV series and the Sebastian segments from the Marsupilami series.</p>
+                <p>This ducky's favorite snack are Strawberries</p>
             </div>
             <div class="grid-item">
-                <h1>Cowboy Duck</h1>
+                <h1>Art Duck</h1>
                 <img class="grid-img" src="./assets/img/Art-Duck.png" alt="Rubber Duck that looks like an artist">
-                <p>Ursula, also known as The Sea Witch or Vanessa, is the main antagonist of the 2023 musical fantasy film The Little Mermaid, the live-action remake of the 1989 animated feature film of the same name. She is a cecaelia sea witch and the younger sister of King Triton.</p>
+                <p>This ducky's favorite snack is Cheese and Crackers</p>
             </div>
             <div class="grid-item">
-                <h1>Barbie Duck</h1>
+                <h1>OG Rubber Duck</h1>
                 <img class="grid-img" src="./assets/img/Original-Duck.png" alt="Rubber Duck that looks like the original rubber duck">
-                <p>Ursula, also known as The Sea Witch or Vanessa, is the main antagonist of the 2023 musical fantasy film The Little Mermaid, the live-action remake of the 1989 animated feature film of the same name. She is a cecaelia sea witch and the younger sister of King Triton.</p>
+                <p>This ducky's favorite food is Scrambled Eggs</p>
             </div>
             <div class="grid-item">
-                <h1>Cowboy Duck</h1>
+                <h1>Airforce Officer Duck</h1>
                 <img class="grid-img" src="./assets/img/Airforce-Duck.png" alt="Rubber Duck that looks like a US Airforce Officer">
-                <p>Ursula, also known as The Sea Witch or Vanessa, is the main antagonist of the 2023 musical fantasy film The Little Mermaid, the live-action remake of the 1989 animated feature film of the same name. She is a cecaelia sea witch and the younger sister of King Triton.</p>
+                <p>This ducky's favorite foods are Meatloaf and Chicken</p>
             </div>
         </div>
     </main>
